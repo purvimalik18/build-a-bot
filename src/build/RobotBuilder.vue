38a86c49d3dbb91eb004de7b8c47acf496d28d63
Malik <purvi.malik@capgemini.com>
2022-10-10 15:15:18 +0530

    passing data to parent components with events

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,59 +1,94 @@
-<!-- eslint-disable linebreak-style -->
 <template>
-<div class="content">
-  <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+  <div class="content">
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src" alt=""/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left" alt=""/>
+          <img :src="selectedRobot.torso.src" alt=""/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right" alt=""/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src" alt=""/>
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()">Add to cart</button>
+    </div>
     <div class="top-row">
-      <!-- binding inline class -->
-        <!-- <div class="top part" :style="headBorderStyle"> -->
-          <!-- binding externally class -->
-        <!-- <div :class="[saleBorderClass, 'top', 'part']">
-          <div class="robot-name">
-            {{selectedRobot.head.title}}
-            <span class="sale" v-if="selectedRobot.head.onSale">Sale!</span>
-          </div>
+      <div
+        class="top part"
+        :style="headBorderStyle"
+        :class="{ 'sale-boarder': selectedRobot.head.onSale }"
+      >
+        <!-- <div class="robot-name">
+          {{ selectedRobot.head.title }}
+          <span class="sale" v-if="selectedRobot.head.onSale">Sale!</span>
         </div> -->
-        <PartSelector :parts="availableParts.heads" position="top"/>
+        <PartSelector
+          :parts="availableParts.heads"
+          position="top"
+          @partSelected="part => (selectedRobot.head = part)"
+        />
+      </div>
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms" position="left"/>
-      <PartSelector :parts="availableParts.torsos" position="center"/>
-      <PartSelector :parts="availableParts.arms" position="right"/>
+      <PartSelector
+        :parts="availableParts.arms"
+        position="left"
+        @partSelected="part => (selectedRobot.leftArm = part)"
+      />
+      <PartSelector
+        :parts="availableParts.torsos"
+        position="center"
+        @partSelected="part => (selectedRobot.torso = part)"
+      />
+      <PartSelector
+        :parts="availableParts.arms"
+        position="right"
+        @partSelected="part => (selectedRobot.rightArm = part)"
+      />
     </div>
     <div class="bottom-row">
-        <PartSelector :parts="availableParts.bases" position="bottom"/>
+      <PartSelector
+        :parts="availableParts.bases"
+        position="bottom"
+        @partSelected="part => (selectedRobot.base = part)"
+      />
     </div>
+
     <div>
       <h1>Cart</h1>
       <table>
         <thead>
           <tr>
             <th>Robot</th>
-            <th class="cost">Cast</th>
+            <th class="cost">cost</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(robot, index) in cart" :key="index">
-            <td>{{robot.head.title}}</td>
-            <td class="cost">{{robot.cost}}</td>
+            <td>{{ robot.head.title }}</td>
+            <td>{{ robot.cost }}</td>
+          </tr>
+          <tr>
+            <td>Total</td>
+            <td>{{ totalCost }}</td>
           </tr>
         </tbody>
       </table>
     </div>
-</div>
+  </div>
 </template>
-<!-- eslint-disable linebreak-style -->
 
 <script>
+/* eslint linebreak-style: ["error", "windows"] */
 import availableParts from '../data/parts';
-import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
 
 export default {
   name: 'RobotBuilder',
-  // example of lifecycle hooks
-  // created() {
-  //   console.log('created component');
-  // },
   components: { PartSelector },
   data() {
     return {
@@ -62,159 +97,106 @@ export default {
       selectedRobot: {
         head: {},
         leftArm: {},
-        torsos: {},
+        torso: {},
         rightArm: {},
-        bases: {},
+        base: {},
       },
     };
   },
-  mixins: [createdHookMixin],
   computed: {
-    saleBorderClass() {
-      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    totalCost() {
+      return this.cart.length > 0
+        ? this.cart.reduce((total, robot) => total + robot.cost, 0)
+        : 0;
     },
     headBorderStyle() {
-      return { border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa' };
+      return {
+        border: this.selectedRobot.head.onSale
+          ? '3px solid red'
+          : '3px solid grey',
+      };
     },
   },
   methods: {
     addToCart() {
       const robot = this.selectedRobot;
-      const cost = robot.head.cost + robot.leftArm.cost
-                + robot.rightArm.cost + robot.bases.cost + robot.torsos.cost;
-      this.cart.push({ ...robot, cost });
+      const cost = robot.head.cost
+        + robot.leftArm.cost
+        + robot.rightArm.cost
+        + robot.torso.cost
+        + robot.base.cost;
+      this.cart.push({ ...this.selectedRobot, cost });
     },
   },
 };
 </script>
-<!-- eslint-disable linebreak-style -->
 
-<style scoped lang="scss">
-    .part {
-        position: relative;
-        width:165px;
-        height:165px;
-        border: 3px solid #aaa;
-      }
-      .part{
-        img {
-          width:165px;
-        }
-      }
-      .top-row {
-        display:flex;
-        justify-content: space-around;
-      }
-      .middle-row {
-        display:flex;
-        justify-content: center;
-      }
-      .bottom-row {
-        display:flex;
-        justify-content: space-around;
-        border-top: none;
-      }
-      .head {
-        border-bottom: none;
-      }
-      .left {
-        border-right: none;
-      }
-      .right {
-        border-left: none;
-      }
-      .left img {
-        transform: rotate(-90deg);
-      }
-      .right img {
-        transform: rotate(90deg);
-      }
-      .bottom {
-        border-top: none;
-      }
-      .prev-selector {
-        position: absolute;
-        z-index:1;
-        top: -3px;
-        left: -28px;
-        width: 25px;
-        height: 171px;
-      }
-      .next-selector {
-        position: absolute;
-        z-index:1;
-        top: -3px;
-        right: -28px;
-        width: 25px;
-        height: 171px;
-      }
-      .center .prev-selector, .center .next-selector {
-        opacity:0.8;
-      }
-      .left .prev-selector {
-        top: -28px;
-        left: -3px;
-        width: 144px;
-        height: 25px;
-      }
-      .left .next-selector {
-        top: auto;
-        bottom: -28px;
-        left: -3px;
-        width: 144px;
-        height: 25px;
-      }
-      .right .prev-selector {
-        top: -28px;
-        left: 24px;
-        width: 144px;
-        height: 25px;
-      }
-      .right .next-selector {
-        top: auto;
-        bottom: -28px;
-        left: 24px;
-        width: 144px;
-        height: 25px;
-      }
-      .right .next-selector {
-        right: -3px;
-      }
+<style scoped>
+.top-row {
+  display: flex;
+  justify-content: space-around;
+}
+.middle-row {
+  display: flex;
+  justify-content: center;
+}
+.bottom-row {
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
+}
 
-      .robot-name{
-        position: absolute;
-        top: -25px;
-        text-align: center;
-        width: 100% ;
-      }
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
+}
 
-      .sale{
-        color: red;
-      }
+.content {
+  position: relative;
+}
+.add-to-cart {
+  position: absolute;
+  width: 210px;
+  padding: 3px;
+  font-size: 16px;
+}
 
-      .content{
-        position: relative;
-      }
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
 
-      .add-to-cart{
-        position: absolute;
-        right: 30px;
-        width: 220px;
-        padding: 3px;
-        font-size: 16px;
-      }
+tr {
+  text-align: right;
+}
 
-      td,th{
-        text-align: left;
-        padding: 5px;
-        padding-right: 20px;
-      }
+.sale-boarder {
+  border: 3px solid red;
+}
 
-      .cost{
-        text-align: right;
-      }
-
-      .sale-border{
-        border: 3px solid red;
-      }
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
+}
 </style>
